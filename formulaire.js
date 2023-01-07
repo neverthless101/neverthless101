c6f1fb6b66fa3dec7ca775ef3fd3795a1e359056
ayatell <maya.addy123@gmail.com>
2023-01-07 21:38:15 +0100

    added 64 file

---
+++ formulaire.js
@@ -0,0 +1,13 @@
+function verif()
+{
+if(formulaire.nom.value == '') // s'il manque le nom, affiche l'alerte !
+alert('Veuillez saisir le nom');
+else if(formulaire.msg.value == '') // s'il manque le prénom, affiche l'alerte !
+alert('Merci de saisir le commentaire!');
+else if(formulaire.nom.value == '' && formulaire.msg.value == ''); // si tous les champs sont vides! affiche l'alerte!
+alert('Veuillez remplir les champs !');
+formulaire.go.click() = String.link(url( <a href="music.html"></a>));
+}
+
+
+alert('Bienvenue à bord!!');
